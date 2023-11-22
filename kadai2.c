942c7a2136557e0c33752f84ab7a789a70de4bc0
Ajmal-0045 <147790036+Ajmal-0045@users.noreply.github.com>
2023-11-22 16:14:04 +0900

    kadai 2 and 3
    adding kadai2, kadai2.c, kadai3, kadai3.c

---
+++ kadai2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+    int x = 10;
+    int y = 2*x;
+
+    int z = x+y;
+    printf("x=%d,y=%d,z=%d\n",x,y,z);
+}
---
+++ kadai3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+
+    int price = 500;
+    float y = 1.1*price;
+
+    printf("result=%6.2f\n",y);
+}
